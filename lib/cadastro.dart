d3ad25ec46ed5dc16885e5beb6e009a9e55ac747
Kaio C. de Oliveira <hello@kaio.dev>
2022-06-16 18:07:07 -0300

    feature: adiciona cadastro

---
+++ lib/cadastro.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Cadastro extends StatelessWidget {
+  const Cadastro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Cadastro");
+  }
+}
