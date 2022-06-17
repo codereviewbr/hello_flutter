3d2152d0679201169ff1e46a40373edb1f4964b9
Renato da Costa Nunes <wheelingrenato@gmail.com>
2022-06-17 18:12:05 -0300

    adiciona recuperar senha

---
+++ lib/recuperar_senha.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class RecuperarSenha extends StatelessWidget {
+  const RecuperarSenha({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Recuperar senha");
+  }
+}
